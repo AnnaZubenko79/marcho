b05bce875f5ef96c33f6cbed3261ade4d71e3a81
AnnaZubenko79 <annazubenko79@gmail.com>
2021-05-31 20:59:11 +0300

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,15 @@
 $(function(){
 $('.top-slider__inner').slick({
     
-})
+});
+
+$(".star").rateYo({
+    
+    starWidth: "17px",
+    normallFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true
+
+  });
+
 });
