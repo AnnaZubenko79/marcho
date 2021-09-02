7597d683d9c98c65b6c5853a495c37da67caca97
AnnaZubenko79 <annazubenko79@gmail.com>
2021-09-02 10:48:59 +0300

    finished without adaptiv

--- app/js/main.js
+++ app/js/main.js
@@ -123,12 +123,6 @@ function initializeClock(id, endtime) {
 const deadline = $('.promo__clock').attr('data-time');
 initializeClock('promo__clock', deadline);
 
-// let map;
-// function initMap() { 
-//   const map = new google.maps.Map(document.getElementById("map"), {
-//     zoom: 8,
-//     center: { lat: -34.397, lng: 150.644 },
-//   });
-// }
+
 
 
